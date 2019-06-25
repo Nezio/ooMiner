368a1308fab05958acf1ad86aa982c383e4ca0e3
Nezio <nemanjaz1994@gmail.com>
2019-06-26 00:53:36 +0200

    Added basic player movement and a few other scripts.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        { // move forward
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        { // move backwards
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        { // move left
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        { // move right
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        }
+    }
+}
