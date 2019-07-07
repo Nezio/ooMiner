35ac42b686c92d65e97cb623c79b0530bb2ee667
Nezio <nemanjaz1994@gmail.com>
2019-07-07 17:08:32 +0200

    Fixed player not moving back during level move because of lerp. Fixed player moving too much back during level move if player is child of a platform.

--- Assets/Scripts/LevelMover.cs
+++ Assets/Scripts/LevelMover.cs
@@ -10,37 +10,61 @@ public class LevelMover : MonoBehaviour
     public GameObject player;
 
     private Camera mainCamera;
-    
+    private Player playerScript;
+
+    private bool moving = false;
+
     private void Start()
     {
         mainCamera = Camera.main;
+        playerScript = player.GetComponent<Player>();
     }
 
     private void Update()
     {
-        // if camera is far from origin call level move fn
-        if(mainCamera.transform.position.z >= maxLevelDistance)
+        // if not already in the moving process and camera is far from origin call level move fn
+        if(!moving && mainCamera.transform.position.z >= maxLevelDistance)
         {
-            MoveLevel();
+            StartCoroutine(MoveLevel());
         }
     }
     
-    private void MoveLevel()
+    private IEnumerator MoveLevel()
     {
+        moving = true;
+
+        // if player is moving wait for frame where he's not
+        while (playerScript.IsMoving())
+        {
+            yield return new WaitForEndOfFrame();
+        } // MOVE THE LEVEL
+
+
+        // move distance is now used instead of maxLevelDistance because camera may move a bit while player stops moving
+        int moveDistance = Mathf.FloorToInt(mainCamera.transform.position.z);
+
+        // move player only if player is not child of some platform of a strip 
+        if (player.transform.parent == null)
+        {
+            Vector3 playerLocPos = player.transform.localPosition;
+            player.transform.position = new Vector3(playerLocPos.x, playerLocPos.y, playerLocPos.z - moveDistance);
+        }
+
         // move strips
         GameObject[] strips = level.GetStrips();
         for(int i = 0; i < strips.Length; i++)
         {
             Vector3 stripLocPos = strips[i].transform.localPosition;
-            strips[i].transform.localPosition = new Vector3(stripLocPos.x, stripLocPos.y, stripLocPos.z - maxLevelDistance);
+            strips[i].transform.localPosition = new Vector3(stripLocPos.x, stripLocPos.y, stripLocPos.z - moveDistance);
         }
-
-        // move player
-        Vector3 playerLocPos = player.transform.localPosition;
-        player.transform.position = new Vector3(playerLocPos.x, playerLocPos.y, playerLocPos.z - maxLevelDistance);
-
+        
         // move camera
         Vector3 cameraLocPos = mainCamera.transform.localPosition;
-        mainCamera.transform.position = new Vector3(cameraLocPos.x, cameraLocPos.y, cameraLocPos.z - maxLevelDistance);
+        mainCamera.transform.position = new Vector3(cameraLocPos.x, cameraLocPos.y, cameraLocPos.z - moveDistance);
+
+
+        moving = false;
+
+        yield return null;
     }
 }
